408d59a3f891189ab080d6c7e9188c7e75bb6b44
Nhat Vu <nhat.vu@employmenthero.com>
2022-12-06 00:57:22 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   darkMode: "class",
   theme: {
     screens: {
-      mob: "375px",
-      tablet: "768px",
-      laptop: "1024px",
-      desktop: "1280px",
-      laptopl: "1440px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
       print: { raw: "print" },
     },
     extend: {},
